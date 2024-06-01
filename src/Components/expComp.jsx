8439deeb724c1779f2e8632c42667a26bb27d852
Ni3-Jadhav <ntjadhav2000@gmail.com>
2024-06-01 12:15:50 +0530

    initial warm up for project

---
+++ src/Components/expComp.jsx
@@ -0,0 +1,10 @@
+
+ const expComp =()=>{
+    return(
+        <>
+        <p>hhi</p>
+        <img src={`${process.env.PUBLIC_URL}/logo512.png`} alt="" />
+        </>
+    )
+}
+export default expComp;
---
+++ src/Pages/expComp.js
@@ -0,0 +1,11 @@
+import '../App.css';
+import myimg from '../images/nitin.jpg'
+const Exppage =()=>{
+    return(
+        <>
+        <p>hhi</p>
+        <img src={myimg} alt="" />
+        </>
+    )
+}
+export default Exppage;
